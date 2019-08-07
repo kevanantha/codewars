9877da5129c7f556e4c497320e9110bdea375785
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-08 00:38:23 +0700

    :rocket:

---
+++ yourOrder.js
@@ -0,0 +1,18 @@
+function order(words) {
+  if (!words) return ''
+
+  const array = []
+  words = words.split(' ')
+  let j = 1
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].indexOf(j) > -1) {
+      array.push(words[i])
+      j++
+      i = -1
+    }
+  }
+  return array.join(' ')
+}
+
+console.log(order('is2 Thi1s T4est 3a')) // "Thi1s is2 3a T4est"
+console.log(order('4of Fo1r pe6ople g3ood th5e the2')) // "Fo1r the2 g3ood 4of th5e pe6ople"
