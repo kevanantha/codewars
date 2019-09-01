57b9b87b9f575d00f25393b223a0bbbb200944f6
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-01 23:37:51 +0700

    :rocket:

---
+++ 4kyu/convertQueryToMap.js
@@ -0,0 +1,57 @@
+// Converts a URL Query String into an object map
+function convertQueryToMap(query) {
+  query = query.split('&')
+  let result = []
+
+  for (let i = 0; i < query.length; i++) {
+    const splitEq = query[i].split('=')
+    const key = splitEq[0].split('.')
+    const value = splitEq[1] ? decodeURIComponent(splitEq[1]) : splitEq[1]
+    let temp = { [key[key.length - 1]]: value }
+    for (let j = key.length - 2; j >= 0; j--) {
+      temp = { [key[j]]: temp }
+    }
+    result.push(temp)
+  }
+  const final = mergeObjects({}, ...result)
+
+  return final
+}
+
+function mergeObjects(target) {
+  for (let i = 1; i < arguments.length; i++) {
+    let from = arguments[i]
+    if (typeof from !== 'object') continue
+    for (let j in from) {
+      if (from.hasOwnProperty(j)) {
+        target[j] =
+          typeof from[j] === 'object'
+            ? mergeObjects({}, target[j], from[j])
+            : from[j]
+      }
+    }
+  }
+
+  return target
+}
+
+console.log(
+  convertQueryToMap(
+    'user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue'
+  )
+)
+
+// Converts a URL Query String into an object map
+function convertQueryToMap(query) {
+  var obj = {}
+  query.split('&').map(function(params) {
+    var parts = params.split('=')
+    if (!parts[1]) return {}
+    parts[0].split('.').reduce(function(cur, next, i, arr) {
+      if (!cur[next]) cur[next] = {}
+      if (i === arr.length - 1) cur[next] = decodeURIComponent(parts[1])
+      return cur[next]
+    }, obj)
+  })
+  return obj
+}
--- 4kyu/factorial.js
+++ 4kyu/factorial.js
@@ -1,7 +1,4 @@
-const factorial = n => {
-  if (!n) return '1'
-  return (n * factorial(n - 1)).toString()
-}
+const factorial = n => (!n ? 1 : n * factorial(n - 1))
 
 console.log(factorial(1))
 console.log(factorial(5))
@@ -10,37 +7,37 @@ console.log(factorial(15))
 
 // ===
 
-function factorial(n) {
-  // Add some code
-  if (n < 0) {
-    return null
-  }
+// function factorial(n) {
+//   // Add some code
+//   if (n < 0) {
+//     return null
+//   }
 
-  var factArray = [1]
-  var factSize = 1
-  var carry
-  var prod
-  var digit
-  for (var x = 2; x <= n; x++) {
-    carry = 0
-    for (var i = 0; i < factArray.length; i++) {
-      prod = factArray[i] * x + carry
-      digit = prod % 10
-      carry = Math.floor(prod / 10)
-      factArray[i] = digit
-      if (i === factArray.length - 1) {
-        while (carry > 0) {
-          digit = carry % 10
-          carry = Math.floor(carry / 10)
-          factArray.push(digit)
-        }
-        break
-      }
-    }
-  }
-  var strFactorial = ''
-  for (var i = factArray.length - 1; i >= 0; i--) {
-    strFactorial += '' + factArray[i]
-  }
-  return strFactorial
-}
+//   var factArray = [1]
+//   var factSize = 1
+//   var carry
+//   var prod
+//   var digit
+//   for (var x = 2; x <= n; x++) {
+//     carry = 0
+//     for (var i = 0; i < factArray.length; i++) {
+//       prod = factArray[i] * x + carry
+//       digit = prod % 10
+//       carry = Math.floor(prod / 10)
+//       factArray[i] = digit
+//       if (i === factArray.length - 1) {
+//         while (carry > 0) {
+//           digit = carry % 10
+//           carry = Math.floor(carry / 10)
+//           factArray.push(digit)
+//         }
+//         break
+//       }
+//     }
+//   }
+//   var strFactorial = ''
+//   for (var i = factArray.length - 1; i >= 0; i--) {
+//     strFactorial += '' + factArray[i]
+//   }
+//   return strFactorial
+// }
