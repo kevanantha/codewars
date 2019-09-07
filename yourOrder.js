5e67b50a0b5c8dca4c30fcde86f17b3252db7e12
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-07 16:11:43 +0700

    :alien:

--- yourOrder.js
+++ yourOrder.js
@@ -26,4 +26,5 @@ function order(words) {
 }
 
 console.log(order('is2 Thi1s T4est 3a')) // "Thi1s is2 3a T4est"
+console.log(order('is2 T4est 3a')) // "Thi1s is2 3a T4est"
 console.log(order('4of Fo1r pe6ople g3ood th5e the2')) // "Fo1r the2 g3ood 4of th5e pe6ople"
